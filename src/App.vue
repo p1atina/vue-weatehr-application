9f963c97d9323dd515b4e8a61ce6d330b61292f1
Rudolf <stylish0631@gmail.com>
2022-03-18 23:06:34 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderDisplay />
+  <WeatherContent />
+</template>
+
+<script>
+import HeaderDisplay from "@/components/HeaderDisplay";
+import WeatherContent from "@/components/weather/WeatherContent";
+export default {
+  name: 'App',
+  components: {
+    HeaderDisplay,
+    WeatherContent,
+
+  }
+}
+</script>
+
+<style>
+</style>
